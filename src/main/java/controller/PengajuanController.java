a882c6eb0ddbc566bda2348a5290aff94bbb415e
Ripqii <rifqy.syahdi0606@gmail.com>
2026-09-22 08:24:43 +0800

    first commit

--- src/main/java/controller/PengajuanController.java
+++ src/main/java/controller/PengajuanController.java
@@ -13,10 +13,10 @@ public class PengajuanController {
 
     private void muatDataAwal() {
         Pemohon pemohon1 = new Pemohon("Yahya Jailani", "Biro EDEN");
-        PengajuanDigital p1 = new PengajuanDigital(1, pemohon1, "Poster INSTAND", "09-09-2026", "Instagram Feed", "PNG");
+        PengajuanDigital p1 = new PengajuanDigital(1, pemohon1, "Poster INSTAND", "09/09/2026", "Instagram Feed", "PNG");
 
         Pemohon pemohon2 = new Pemohon("Ahmad Ahdasuki", "Departemen PSD");
-        PengajuanCetak p2 = new PengajuanCetak(2, pemohon2, "Sertifikat ISC", "17-10-2026", "A4 Hardpaper", 50);
+        PengajuanCetak p2 = new PengajuanCetak(2, pemohon2, "Sertifikat ISC", "17/10/2026", "A4 Hardpaper", 50);
 
         listPengajuan.add(p1);
         listPengajuan.add(p2);
